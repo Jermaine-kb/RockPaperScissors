180d3683321f4ee7c01f413c60939e9355989421
Atambo Kassim <atambokb@gmail.com>
2024-09-05 18:24:28 +0300

    initial commit with all files

--- Day 5/For Loops with Range/solution.py
+++ Day 5/For Loops with Range/solution.py
@@ -7,6 +7,6 @@ for number in range (1,101):
     elif number % 3 == 0:
         print("Fizz")
     elif number % 5 == 0:
-        print("Fizz")
+        print("Fuzz")
     else:
         print(number)
